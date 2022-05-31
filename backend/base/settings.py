8075419602e30f63789f691867310616dddc5462
Geovana Coelho <geoocoelho@gmail.com>
2022-05-31 20:11:49 -0300

    backend: corheaders

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'corsheaders',
     'rest_framework',
+    'corsheaders',
 
     'users',
     'occurrences'
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'base.urls'
